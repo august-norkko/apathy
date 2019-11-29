186c6665e4354ff295a2b68cf4e8c583f9b341fe
agge <boundlessvolatilecrow@gmail.com>
2019-11-29 18:07:58 +0100

    remove reduntant logging

--- main.go
+++ main.go
@@ -5,20 +5,10 @@ import (
 	"os"
 	"io"
 	"net/http"
-	"github.com/joho/godotenv"
 	"apathy/database"
 )
 
 func main() {
-	file, err := os.OpenFile("logs.log", os.O_CREATE | os.O_APPEND, 0644)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer file.Close()
-
-	mw := io.MultiWriter(os.Stdout, file)
-	log.SetOutput(mw)
-
 	err = godotenv.Load()
 	if err != nil {
 		log.Println(err)
